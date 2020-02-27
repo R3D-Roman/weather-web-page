5ef52cd5825a6c65b465d20d47592d3723a8b91b
R3D-Roman <patrikpappy@gmail.com>
2020-02-27 14:03:03 +0100

    change design

--- js/app.js
+++ js/app.js
@@ -6,6 +6,36 @@ const popUp = document.querySelector(".popup-btn");
 const autoCompleteDiv = document.querySelector(".auto-complete");
 const divAutocomplete = document.querySelector(".div-autocomplete");
 
+// header title animation
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+.add({
+  targets: '.ml9 .letter',
+  duration: 1000,
+})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+
+
+// end header title animation
+
+
+
 // api fetch functions ===================================================================================================
 
 function fetchData(city) {
