8952356bbe9bd5b02637b9a8de217317a6b05719
R3D-Roman <patrikpappy@gmail.com>
2020-03-05 23:33:19 +0100

    update

--- js/pwa.js
+++ js/pwa.js
@@ -1,5 +1,5 @@
-// if('serviceWorker' in navigator){
-//     navigator.serviceWorker.register('https://r3d-roman.github.io/weather-web-page/../sw.js')
-//       .then(reg => console.log('service worker registered', reg))
-//       .catch(err => console.log('service worker not registered', err));
-//   }
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/weather-web-page/../sw.js', {scope: '/weather-web-page/'})
+    .then(reg => console.log('service worker registered', reg))
+    .catch(err => console.log('service worker not registered', err));
+}
