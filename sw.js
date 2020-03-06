c1b0ea9cbbfd46bf71f60783d9ca49d3884a141b
R3D-Roman <patrikpappy@gmail.com>
2020-03-06 11:55:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.addEventListener("install", evt => {
   //console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
-      // console.log("caching shell assets");
+      console.log("caching shell assets", assets);
       cache.addAll(assets);
     })
   );
