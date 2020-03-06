e6b09582a5130aff2b4ec5010df81717e52929dd
R3D-Roman <patrikpappy@gmail.com>
2020-03-06 12:56:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -82,7 +82,7 @@ self.addEventListener("fetch", evt => {
           return caches.open(dynamicCacheName).then(cache => {
             cache.put(evt.request.url, fetchRes.clone());
             // check cached items size
-            limitCacheSize(dynamicCacheName, 35);
+            limitCacheSize(dynamicCacheName, 25);
             return fetchRes;
           });
         })
