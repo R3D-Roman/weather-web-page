521424dceaa22c95e3364b114c2e7ec367e86798
R3D-Roman <patrikpappy@gmail.com>
2020-03-06 12:20:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -72,38 +72,18 @@ self.addEventListener("fetch", evt => {
   if (!(evt.request.url.indexOf("http") === 0)) return; // skip the request. if request is not made with http protocol
 
   evt.respondWith(
-    caches
-      .match(evt.request)
-      .then(cacheRes => {
-        return (
-          cacheRes ||
-          fetch(evt.request).then(fetchRes => {
-            return caches.open(staticCacheName).then(cache => {
-              cache.put(evt.request.url, fetchRes.clone());
-              // check cached items size
-              limitCacheSize(staticCacheName, 35);
-              return fetchRes;
-            });
-          })
-        );
-      })
-  );
-
-  evt.respondWith(
-    caches
-      .match(evt.request)
-      .then(cacheRes => {
-        return (
-          cacheRes ||
-          fetch(evt.request).then(fetchRes => {
-            return caches.open(dynamicCacheName).then(cache => {
-              cache.put(evt.request.url, fetchRes.clone());
-              // check cached items size
-              limitCacheSize(dynamicCacheName, 35);
-              return fetchRes;
-            });
-          })
-        );
-      })
+    caches.match(evt.request).then(cacheRes => {
+      return (
+        cacheRes ||
+        fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 35);
+            return fetchRes;
+          });
+        })
+      );
+    })
   );
 });
